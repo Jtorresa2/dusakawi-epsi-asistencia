23b4770f189274fc844a5edc732a3e15cd167c61
Luis Pinto <dapin46@hotmail.com>
2026-08-14 17:51:25 -0500

    getUserByUsername can return null

--- backend/src/modules/users/domain/repositories/user-repository.ts
+++ backend/src/modules/users/domain/repositories/user-repository.ts
@@ -2,5 +2,5 @@ import { GenericRepository } from '@shared/repositories/generic-repository.js';
 import { User } from '../entities/user.js';
 
 export interface UserRepository extends GenericRepository<User> {
-  getUserByUsername(username: string): Promise<User>;
+  getUserByUsername(username: string): Promise<User | null>;
 }
