6ac10b1bf8a51c1bdfb1cdaf87aa9d64cd22ead7
Luis Pinto <dapin46@hotmail.com>
2026-08-16 20:12:40 -0500

    add: prisma orm installed and configured

---
+++ backend/src/config/database/prisma/prisma.ts
@@ -0,0 +1,11 @@
+import 'dotenv/config';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from './generated/client.js';
+import { Environment } from '../../environment.js';
+
+const connectionString = `${Environment.DB_URL}`;
+
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter });
+
+export { prisma };
