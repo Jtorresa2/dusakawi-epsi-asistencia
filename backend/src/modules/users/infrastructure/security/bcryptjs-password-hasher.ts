e35157b995c60c0da6557914d3737ae4faead0a3
Luis Pinto <dapin46@hotmail.com>
2026-08-21 00:15:13 -0500

    add: implementations for security interfaces added (token and hash)

---
+++ backend/src/modules/users/infrastructure/security/bcryptjs-password-hasher.ts
@@ -0,0 +1,16 @@
+import bcrypt from 'bcryptjs';
+import type { PasswordHasher } from '../../domain/interfaces/password-hasher.js';
+import { HashedPassword } from '../../domain/value-objects/hashed-password.js';
+
+export class BcryptjsPasswordHasher implements PasswordHasher {
+  private readonly saltRounds = 10;
+
+  async hash(password: string): Promise<HashedPassword> {
+    const hash = await bcrypt.hash(password, this.saltRounds);
+    return HashedPassword.create(hash);
+  }
+
+  async verify(password: string, hash: HashedPassword): Promise<boolean> {
+    return bcrypt.compare(password, hash.value);
+  }
+}
